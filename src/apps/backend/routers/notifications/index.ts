7ba42c7ea90cc13461bca05fdbf3a98cb7c9f4af
laurynas-karvelis-guardian <laurynas.karvelis@guardian.co.uk>
2026-07-10 13:48:23 +0100

    Add a express zod validation middleware and example zod schema

--- src/apps/backend/routers/notifications/index.ts
+++ src/apps/backend/routers/notifications/index.ts
@@ -1,9 +1,25 @@
 import { type Request, type Response, Router } from "express";
+import { notificationPushRequestSchema } from "../../schemas/notification-push";
+import { validate } from "../../utils/middleware/validator";
 
 const router = Router();
 
 router.get("/", (_req: Request, res: Response) => {
   res.json([{ id: 1, message: "You have a new notification!" }]);
 });
+
+router.post(
+  "/",
+  validate(notificationPushRequestSchema),
+  (req: Request, res: Response) => {
+    const { message } = req.body;
+
+    if (!message) {
+      return res.status(400).json({ error: "Message is required" });
+    }
+
+    res.status(201).json({ id: Date.now(), message });
+  },
+);
 
 export default router;
