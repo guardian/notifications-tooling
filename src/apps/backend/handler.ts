926f0fbfa209f48e716e7d42afec94112355a26b
Simona Spiridon <simona.spiridon@guardian.co.uk>
2026-07-21 14:14:35 +0100

    remove pino logging

--- src/apps/backend/handler.ts
+++ src/apps/backend/handler.ts
@@ -1,14 +1,11 @@
 import serverlessExpress from '@codegenie/serverless-express';
 import { env } from '../../packages/config';
 import { app } from './placeholder-app';
-import { logger } from './utils/logger';
 
 const IS_RUNNING_LOCALLY = !process.env.LAMBDA_TASK_ROOT;
 
 if (IS_RUNNING_LOCALLY) {
-	app.listen(env.PORT, env.HOST, () => {
-		logger.info(`🚀 Server running on http://${env.HOST}:${env.PORT}`);
-	});
+	app.listen(env.PORT, env.HOST, () => {});
 }
 
 export const handler = serverlessExpress({ app });
