6bb00a59b5d3c2764620ebb044222ff67c031f3a
DBlatcher <david.blatcher@guardian.co.uk>
2026-07-23 17:26:56 +0100

    undo css change

--- src/apps/frontend/src/App.tsx
+++ src/apps/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-// import './index.css';
+import './index.css';
 
 import { DispatchPage } from './features/dispatch/DispatchPage';
 import { EmailNotificationPage } from './features/stand-frontend/EmailNotificationPage';
