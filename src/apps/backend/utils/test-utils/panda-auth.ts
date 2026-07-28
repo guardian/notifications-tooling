fdcae53a3af448e09f3c0058c95d7d006c96e328
laurynas-karvelis-guardian <laurynas.karvelis@guardian.co.uk>
2026-07-28 11:56:28 +0100

    Fix lint error

--- src/apps/backend/utils/test-utils/panda-auth.ts
+++ src/apps/backend/utils/test-utils/panda-auth.ts
@@ -18,7 +18,7 @@ export const verifyCookieMock = mock<
 >(() => Promise.resolve({ success: false }));
 
 export const installPandaAuthMock = (): void => {
-	mock.module('../auth/pan-domain-authentication', () => ({
+	void mock.module('../auth/pan-domain-authentication', () => ({
 		verifyCookie: verifyCookieMock,
 	}));
 };
