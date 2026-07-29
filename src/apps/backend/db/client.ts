219b31484ce06f0acd82bc43370c0b76a02c9642
Simona Spiridon <simona.spiridon@guardian.co.uk>
2026-07-29 15:56:12 +0100

    add schema + drizzle

---
+++ src/apps/backend/db/client.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error('DATABASE_URL is required');
+}
+
+const pool = new Pool({
+    connectionString,
+});
+
+
+export const db = drizzle({ client: pool });
+export { pool };
