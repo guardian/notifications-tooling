be1eb9b6a9b9f6397a3a01fbb76d2f8e55bcc65f
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2026-08-04 12:34:30 +0100

    remove auth from api documentation

--- src/apps/backend/routers/docs/index.ts
+++ src/apps/backend/routers/docs/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
-import { authRedirectMiddleware } from '../../middleware/auth-middleware';
+// import { authRedirectMiddleware } from '../../middleware/auth-middleware';
 import { openApiDocument } from './openapi';
 
 export const docsRouter = Router();
-docsRouter.use(authRedirectMiddleware);
+// docsRouter.use(authRedirectMiddleware);
 
 docsRouter.use('/', swaggerUi.serve);
 docsRouter.get('/', swaggerUi.setup(openApiDocument));
