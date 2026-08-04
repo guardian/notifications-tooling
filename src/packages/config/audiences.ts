6c06d734f8a8720d3860f0ce3fe65c4c99acc7c0
Marcin Gosz <marcin.gosz@theguardian.com>
2026-08-04 12:37:41 +0100

    updating code newsletter ids

--- src/packages/config/audiences.ts
+++ src/packages/config/audiences.ts
@@ -20,17 +20,17 @@ const newsletterSegmentsByStage = {
 		UK: {
 			label: 'UK',
 			brazeCampaignId: 'da019800-869e-4e1d-9c2e-029741829af1',
-			emailRenderingNewsletterId: 'breaking-news-us',
+			emailRenderingNewsletterId: 'breaking-news-uk',
 		},
 		US: {
 			label: 'US',
 			brazeCampaignId: 'a945e3ae-165b-46d7-b163-0ca1c6beb2f4',
-			emailRenderingNewsletterId: 'breaking-news-us',
+			emailRenderingNewsletterId: 'breakingnewsus',
 		},
 		AU: {
 			label: 'AU',
 			brazeCampaignId: '5da1b754-42f4-440d-9eec-0d595190a0f0',
-			emailRenderingNewsletterId: 'breaking-news-us',
+			emailRenderingNewsletterId: 'breaking-news-au',
 		},
 	},
 	PROD: {
