0737989e533d76184ae01ba0eadc6786f9d4e48e
Simona Spiridon <simona.spiridon@guardian.co.uk>
2026-08-06 13:20:14 +0100

    format

--- src/packages/database/client.ts
+++ src/packages/database/client.ts
@@ -1,13 +1,13 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { getConnectionString } from './config-loader';
+
 type Db = ReturnType<typeof drizzle>;
 
 let poolPromise: Promise<Pool> | undefined;
 let dbPromise: Promise<Db> | undefined;
 
-
- const getPool = async (): Promise<Pool> => {
+const getPool = async (): Promise<Pool> => {
 	poolPromise ??= (async () => {
 		const connectionString = await getConnectionString();
 		return new Pool({ connectionString });
