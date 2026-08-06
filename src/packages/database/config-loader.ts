5748b58ea94c85fcddbec591b95b5b15b5400e68
Simona Spiridon <simona.spiridon@guardian.co.uk>
2026-08-06 11:18:21 +0100

    separate responsibilities, add config loader to build the urls

---
+++ src/packages/database/config-loader.ts
@@ -0,0 +1,66 @@
+import { isRunningLocally } from '@config';
+import { getSSMParameter } from '@config/ssm';
+import { logger } from '@http-logger';
+import { z } from 'zod';
+
+// This schema matches the managed database credentials stored in Secrets Manager
+const dbConfigSchema = z.object({
+	host: z.string(),
+	port: z.number(),
+	dbname: z.string(),
+	username: z.string(),
+	password: z.string(),
+});
+type DBConfigSchema = z.infer<typeof dbConfigSchema>;
+
+const getDbConfig = async () => {
+	try {
+		const result = await getSSMParameter('db', true);
+		const parsed = dbConfigSchema.safeParse(result);
+
+		if (!parsed.success) {
+			throw new Error('Could not parse database config from SSM parameter');
+		}
+
+		return parsed.data;
+	} catch (error) {
+		logger.error(
+			{ error },
+			'Error fetching database config from SSM parameter',
+		);
+		throw error;
+	}
+};
+
+const getLocalDbConfig = (): DBConfigSchema => {
+	const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+
+	if (!DB_HOST || !DB_PORT || !DB_NAME || !DB_USERNAME || !DB_PASSWORD) {
+		throw new Error(
+			'Missing required database environment variables for local development.',
+		);
+	}
+
+	const parsedDBPort = parseInt(DB_PORT);
+	if (isNaN(parsedDBPort)) {
+		throw new Error('DB_PORT environment variable must be a valid number.');
+	}
+
+	return {
+		host: DB_HOST,
+		port: parsedDBPort,
+		dbname: DB_NAME,
+		username: DB_USERNAME,
+		password: DB_PASSWORD,
+	};
+};
+
+export const getConnectionString = async () => {
+	const config: DBConfigSchema = isRunningLocally
+		? getLocalDbConfig()
+		: await getDbConfig();
+
+	const { host, port, dbname, username, password } = config;
+
+	return `postgresql://${username}:${password}@${host}:${port}/${dbname}`;
+};
