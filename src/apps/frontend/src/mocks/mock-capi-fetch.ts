34031828df688e00532846978f6d189a730943e0
DBlatcher <david.blatcher@guardian.co.uk>
2026-08-11 14:13:11 +0100

    remove the client-side fetch function

--- src/apps/frontend/src/mocks/mock-capi-fetch.ts
+++ src/apps/frontend/src/mocks/mock-capi-fetch.ts
@@ -12,36 +12,3 @@ export const mockCapiFetch = (articleId: string): Promise<ResolvedArticle> => {
 		}, 500);
 	});
 };
-
-const baseUrl = 'https://content.guardianapis.com';
-const params = 'api-key=test&show-fields=headline,standfirst,thumbnail';
-
-/**
- * TO DO - this is not intended for production use, just to demo.
- */
-export const hackyClientSideCapiFetch = async (
-	articleId: string,
-): Promise<ResolvedArticle> => {
-	const url = `${baseUrl}/${articleId}?${params}`;
-	const response = await fetch(url);
-	if (!response.ok) {
-		throw new Error(`capi fetch failed: ${response.statusText}`);
-	}
-	const json: unknown = await response.json();
-
-	try {
-		const castJson = json as {
-			response: {
-				content: ResolvedArticle;
-				status: string;
-				total: number;
-			};
-		};
-		if (castJson.response.status !== 'ok') {
-			throw new Error(`CAPI returned ${castJson.response.status} response`);
-		}
-		return castJson.response.content;
-	} catch {
-		throw new Error('Could not parse CAPI json');
-	}
-};
