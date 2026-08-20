cc6bf6e5cef27d3eb792dcb2ce21b338ece00609
Simona Spiridon <simona.spiridon@guardian.co.uk>
2026-08-20 11:10:16 +0100

    format

--- src/apps/backend/routers/docs/index.ts
+++ src/apps/backend/routers/docs/index.ts
@@ -1,19 +1,18 @@
-import { Router } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import getAbsoluteSwaggerFsPath from 'swagger-ui-dist/absolute-path';
-import { authRedirectMiddleware } from '../../middleware/auth-middleware';
 import fs from 'node:fs';
 import path from 'node:path';
+import { Router } from 'express';
+import getAbsoluteSwaggerFsPath from 'swagger-ui-dist/absolute-path';
+import swaggerUi from 'swagger-ui-express';
+import { authRedirectMiddleware } from '../../middleware/auth-middleware';
 import { openApiDocument } from './openapi';
 
 const swaggerPath = getAbsoluteSwaggerFsPath();
 
 console.log('Swagger path:', swaggerPath);
 console.log(
-    'Bundle exists:',
-    fs.existsSync(path.join(swaggerPath, 'swagger-ui-bundle.js')),
+	'Bundle exists:',
+	fs.existsSync(path.join(swaggerPath, 'swagger-ui-bundle.js')),
 );
-
 
 export const docsRouter = Router();
 docsRouter.use(authRedirectMiddleware);
