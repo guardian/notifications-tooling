31421c405aa86563404d5de676f33a7ed73b09d8
Simona Spiridon <simona.spiridon@guardian.co.uk>
2026-08-20 16:17:45 +0100

    use regex

--- src/apps/backend/routers/docs/index.ts
+++ src/apps/backend/routers/docs/index.ts
@@ -6,15 +6,7 @@ import { openApiDocument } from './openapi';
 const docsBasePath = '/docs/api/';
 const swaggerHtml = swaggerUi
 	.generateHTML(openApiDocument)
-	.replaceAll('./swagger-ui.css', `${docsBasePath}swagger-ui.css`)
-	.replaceAll('./favicon-32x32.png', `${docsBasePath}favicon-32x32.png`)
-	.replaceAll('./favicon-16x16.png', `${docsBasePath}favicon-16x16.png`)
-	.replaceAll('./swagger-ui-bundle.js', `${docsBasePath}swagger-ui-bundle.js`)
-	.replaceAll(
-		'./swagger-ui-standalone-preset.js',
-		`${docsBasePath}swagger-ui-standalone-preset.js`,
-	)
-	.replaceAll('./swagger-ui-init.js', `${docsBasePath}swagger-ui-init.js`);
+	.replaceAll(/\.\/(?=[\w-]+\.(?:css|js|png))/g, docsBasePath);
 
 export const docsRouter = Router();
 docsRouter.use(authRedirectMiddleware);
