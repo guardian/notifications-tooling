94b056e4b29f159b31b1cb24cbf715738a70d497
DBlatcher <david.blatcher@guardian.co.uk>
2026-08-26 15:09:33 +0100

    chane the labels in the backend

--- src/packages/config/audiences.ts
+++ src/packages/config/audiences.ts
@@ -23,18 +23,18 @@ const newsletterSegmentsByStage: Record<
 > = {
 	CODE: {
 		UK: {
-			label: 'UK',
+			label: 'United Kingdom',
 			brazeCampaignId: 'da019800-869e-4e1d-9c2e-029741829af1',
 			emailRenderingNewsletterId: 'breaking-news-uk',
 		},
 		US: {
-			label: 'US',
+			label: 'United States',
 			brazeCampaignId: 'a945e3ae-165b-46d7-b163-0ca1c6beb2f4',
 			// this is the right id for the CODE version of this newsletter - the PROD version below does have different casing
 			emailRenderingNewsletterId: 'breakingnewsus',
 		},
 		AU: {
-			label: 'AU',
+			label: 'Australia',
 			brazeCampaignId: '5da1b754-42f4-440d-9eec-0d595190a0f0',
 			emailRenderingNewsletterId: 'breaking-news-au',
 		},
