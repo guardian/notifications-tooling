79ce142a2d39ea96e356278a55d99a0cd10dc3f1
Simona Spiridon <simona.spiridon@guardian.co.uk>
2026-09-11 12:06:43 +0100

    apply wrap for all screen sizes

--- src/apps/frontend/src/themes.ts
+++ src/apps/frontend/src/themes.ts
@@ -486,9 +486,7 @@ export const historyViewStyles = {
 		display: 'inline-flex',
 		alignItems: 'center',
 		gap: semanticSpacing.stackXs,
-		'@media (max-width: 680px)': {
-			flexWrap: 'wrap',
-		},
+		flexWrap: 'wrap',
 		'& svg': {
 			display: 'block',
 			width: '24px',
